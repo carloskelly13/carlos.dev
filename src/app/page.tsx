f61c81180d44f6f80f25dc33e853c1226c7be31e
Carlos Kelly <me@carlos.dev>
2023-01-28 14:58:07 -0600

    Added prettier and linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import { Inter } from "@next/font/google"
+import styles from "./page.module.css"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
   return (
     <main className={styles.main}>
-    <h1 className={inter.className}>Carlos Kelly</h1>
-  <h2 className={inter.className}>Software Engineer</h2></main>
+      <h1 className={inter.className}>Carlos Kelly</h1>
+      <h2 className={inter.className}>Software Engineer</h2>
+    </main>
   )
 }
