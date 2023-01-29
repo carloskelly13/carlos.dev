d8a6ce47ec9540d884377f1a1cb78d52befa11d9
Carlos Kelly <me@carlos.dev>
2023-01-29 10:49:18 -0600

    Add some styling.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,32 @@
-import { Inter } from "@next/font/google"
+import { Syne } from "@next/font/google"
 import styles from "./page.module.css"
 import clsx from "clsx"
-import { SiGithub, SiInstagram, SiLinkedin, SiTwitter } from "react-icons/si"
-const inter = Inter({ subsets: ["latin"] })
+
+const syne = Syne({ subsets: ["latin"] })
 
 export default function Home() {
   return (
-    <main className={clsx(styles.main, styles.title, inter.className)}>
-      <h1>Carlos Kelly</h1>
-      <h2>
+    <main className={clsx(styles.main, styles.title, syne.className)}>
+      <div className={styles.banner} />
+      <h1 className={styles.firstName}>CARLOS</h1>
+      <h2 className={styles.lastName}>KELLY</h2>
+      <h3 className={styles.biLine}>
         Software Engineer &amp; CTO at{" "}
         <a href="https://www.formidable.com">Formidable</a>{" "}
-      </h2>
+      </h3>
       <p className={styles.bio}>
-        I am a full-stack software engineer building apps using web and native
-        platform technologies. My toolkit includes React, GraphQL, Node,
-        Headless CMSs, TypeScript, Swift, and Kotlin. I am also the CTO at
-        Formidable, a digital product consultancy specializing in engineering,
-        design, and product development.
+        I build apps using web and native platform technologies. My toolkit
+        includes React, GraphQL, Node, Headless CMS, TypeScript, Swift, and
+        Kotlin. I’m the CTO at Formidable, a global design, product, and
+        engineering consultancy with a focus on open-source software.
       </p>
       <div className={styles.socials}>
-        <a href="https://www.github.com/carlos-kelly">
-          <SiGithub size={32} title="Visit GitHub profile" />
-        </a>
-        <a href="https://www.instagram.com/carloskelly.13">
-          <SiInstagram size={32} title="Visit Instagram profile" />
-        </a>
+        <a href="https://www.github.com/carlos-kelly">GitHub</a>
+        <a href="https://www.instagram.com/carloskelly.13">Instagram</a>
         <a href="https://www.linkedin.com/in/carlos-kelly-a10270b8/">
-          <SiLinkedin size={32} title="Visit LinkedIn profile" />
+          LinkedIn
         </a>
-        <a href="https://twitter.com/carlos_paelinck">
-          <SiTwitter size={32} title="Visit Twitter profile" />
-        </a>
+        <a href="https://twitter.com/carlos_paelinck">Twitter</a>
       </div>
     </main>
   )
