9d754aa28b975226bc2c8b188a5d45ccbc80578c
Carlos Kelly <me@carlos.dev>
2024-03-31 11:26:40 -0500

    Update design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
+import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    icon({
+      include: ["teenyicons", "heroicons", "iconamoon"],
+    }),
+  ],
 })
